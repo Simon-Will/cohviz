d260b828f54c3a20e59829dbda5e7fa9a523ecbd
Simon Will <will@next-kraftwerke.de>
2021-12-27 20:42:21 +0100

    chore: integrate rftagger and make analysis of German work

--- web/cohapp/coherenceanalyzer/constants.py
+++ web/cohapp/coherenceanalyzer/constants.py
@@ -2,19 +2,24 @@
 
 import os
 
-# File_path
-file_path = os.path.dirname(os.path.realpath(__file__))
+this_dir = os.path.dirname(os.path.realpath(__file__))
 
-# RFTagger variables
-tokenizer = file_path + "/RFTagger/cmd/tokenize.perl"
-rftagger = file_path + "/RFTagger/bin/rft-annotate"
-german_par = file_path + "/RFTagger/lib/german.par"
+# RFTagger files
+default_rf_tagger_path = os.path.join(this_dir, "RFTagger")
+rf_tagger_path = os.environ.get("RF_TAGGER_ROOT", default_rf_tagger_path)
+
+tokenizer = os.path.join(rf_tagger_path, "cmd/tokenize.perl")
+rftagger = os.path.join(rf_tagger_path, "bin/rft-annotate")
+german_par = os.path.join(rf_tagger_path, "lib/german.par")
 
 # Temporary files
-temp_text = file_path + "/temp/text"
-temp_tokens = file_path + "/temp/tokens"
-temp_tags = file_path + "/temp/tags"
-temp_dir = file_path + "/temp"
+temp_dir = os.path.join(this_dir, "temp")
+if not os.path.exists(temp_dir):
+    os.makedirs(temp_dir)
+
+temp_text = os.path.join(temp_dir, "text")
+temp_tokens = os.path.join(temp_dir, "tokens")
+temp_tags = os.path.join(temp_dir, "tags")
 
 # Debug files
-debug = file_path + "/temp/debug.txt"
+debug = os.path.join(temp_dir, "debug.txt")
