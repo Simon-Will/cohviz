74be1cd4fc5b055b0302aee86401ff99de743f75
Simon Will <gorgor@gorgor.de>
2022-01-22 20:14:34 +0100

    chore: update settings for newer Django version

--- web/toolapp/settings.py
+++ web/toolapp/settings.py
@@ -10,17 +10,30 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 
+
+def parse_comma_separated_list(s):
+    return [element.strip() for element in s.split(',')]
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(=58t!)5knrdbdwm17qakv#^buy4i0igb+s)7k6$-a(xwt7^e@'
+SECRET_KEY = os.getenv('SECRET_KEY', '(=58t!)5knrdbdwm17qakv#^buy4i0igb+s)7k6$-a(xwt7^e@')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['132.230.54.216', 'tool.ezw.uni-freiburg.de', 'localhost']
+_allowed_hosts = os.getenv('ALLOWED_HOSTS')
+if _allowed_hosts:
+    ALLOWED_HOSTS = parse_comma_separated_list(_allowed_hosts)
+else:
+    ALLOWED_HOSTS = ['localhost']
+
+_csrf_trusted_origins = os.getenv('CSRF_TRUSTED_ORIGINS')
+if _csrf_trusted_origins:
+    CSRF_TRUSTED_ORIGINS = parse_comma_separated_list(_csrf_trusted_origins)
 
 # Application definition
 
@@ -51,7 +64,7 @@ ROOT_URLCONF = 'toolapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -64,8 +77,23 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'toolapp.wsgi.application'
+# Password validation
+# https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
@@ -118,15 +146,10 @@ LOGGING = {
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'Europe/Berlin'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
@@ -148,3 +171,6 @@ REST_FRAMEWORK = {
 
 # https://docs.djangoproject.com/en/3.2/releases/3.2/#customizing-type-of-auto-created-primary-keys
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
+CSRF_COOKIE_SECURE = False
+SESSION_COOKIE_SECURE = False
