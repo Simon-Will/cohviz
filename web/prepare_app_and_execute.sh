5ca5d120582713622616cb8f96fef58091410c59
Simon Will <gorgor@gorgor.de>
2022-01-22 20:15:04 +0100

    chore: run javascript assembling in Dockerfile

--- web/prepare_app_and_execute.sh
+++ web/prepare_app_and_execute.sh
@@ -5,9 +5,6 @@ mkdir -p /data/db
 echo 'Starting mongod in background. Logging to mongod.log'
 bash -c 'mongod 2>&1 | tee mongod.log' &
 
-gulp sass
-gulp webpack
-
 python manage.py migrate
 python manage.py collectstatic --no-input
 
